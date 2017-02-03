15e67b7a4bd4dc3a14387b576afb9f315e83e940
OmairXcluesiv <omair@xcluesiv.com>
2017-02-03 17:52:52 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,22 +1,24 @@
 <?
 
-$html = file_get_contents("http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.XAmerican+Revolutionary+War&_nkw=American+Revolutionary+War&_sacat=0");
-$DOM = new DOMDocument();
-$DOM->loadHTML($html);
-$finder = new DomXPath($DOM);
-$classname = 'vip';
-$nodes = $finder->query("//*[contains(@class, '$classname')]");
-foreach ($nodes as $node) {
-  echo $node->nodeValue;
-}
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+$html = scraperwiki::scrape("http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=American+Revolutionary+War&_pgn=3&_skc=50&rt=nc");
+
+$max_loop = 50;
+for($i=0;$i<=$max_loop;$i++){
+ $dom = new simple_html_dom();
+ $dom->load($html);
+ $r = $dom->find("a.vip");
+ $m = $dom->find("li.lvprice");
+
+  echo $r[$i];
+  echo $m[$i];
+}
+  
 //
 // // Find something on the page using css selectors
 // $dom = new simple_html_dom();
